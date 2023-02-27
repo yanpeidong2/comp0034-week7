f082d4b423c867104cfd35f32bf2b034c88876ae
nicholsons <sarah.sanders@ucl.ac.uk>
2023-02-27 08:17:34 +0000

    setup update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="comp0034-week7",
-    packages=["paralympic_app", "flask_app", "flask_blue", "iris_app"],
+    packages=["paralympic_app", "flask_app", "flask_bp", "iris_app"],
     include_package_data=True,
     install_requires=["flask", "pandas"],
 )
