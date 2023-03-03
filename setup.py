6772b9f1fcfd05b267c87b379c969a78c6d6dd4f
nicholsons <sarah.sanders@ucl.ac.uk>
2023-03-03 15:04:05 +0000

    removed pandas from setup.py as not needed unil week 8

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="comp0034-week7",
     packages=["paralympic_app", "flask_app", "flask_bp", "iris_app"],
     include_package_data=True,
-    install_requires=["flask", "pandas"],
+    install_requires=["flask"],
 )
